272e4b4ee37d638a9aa688ca211c1b065848a959
Bruno Kilian | DeveloperDeck101 <devdeck101@gamil.com>
2024-05-20 10:16:49 -0300

    fix: Saiba mais text

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -47,7 +47,7 @@ export default function Home() {
               <div className="flex flex-col mb-8 lg:mb-16 space-y-4 sm:flex-row sm:justify-center sm:space-y-0 sm:space-x-4">
                 <a
                   href="https://www.youtube.com/playlist?list=PLR8OzKI52ppUa7VP0D671haaiQAkIww3W"
-                  className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900"
+                  className="inline-flex justify-center items-center py-3 px-5 text-primary  font-medium text-center  rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900"
                   target="_blank"
                 >
                   Saiba mais
